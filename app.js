6acfe910429c5c740e2a45e76064b0ffe13e7458
Sunil Poudel <85890714+Khostaa@users.noreply.github.com>
2022-09-04 13:40:12 +0545

    updated timer

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () =>{
-    const countDate = new Date('September 3,2022 14:00:00').getTime();
+    const countDate = new Date('September 23,2022 17:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
